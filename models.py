3132b807d93140e09ed51b5a9c9ae87c7c219bf6
Becous <becousgame@gmail.com>
2024-08-14 20:49:37 +0300

    add more func

--- models.py
+++ models.py
@@ -1,18 +1,6 @@
-from sqlalchemy import Column, Integer, String, Float, CheckConstraint, Boolean
+from sqlalchemy import Column, Integer, String, Float, CheckConstraint, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
-
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
-
-
-# class Recipe(BaseModel):
-#     name: str
-#     rating: conint(ge=1, le=5) # type: ignore
-#     desc: str
-#     time_cook: int
-
 
 class Item(Base):
     __tablename__ = "item"
@@ -21,6 +9,17 @@ class Item(Base):
     name = Column(String)
     price = Column(Float)
 
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    surname = Column(String)
+    password = Column(String)
+    recipe = Column()
+
+    saved_recipe = relationship("SaveRecipe", back_populates="user", cascade="all, delete")
+    comment = relationship("Comment", back_populates="user", cascade="all, delete")
 
 
 class Recipe(Base):
@@ -31,3 +30,30 @@ class Recipe(Base):
     rating = Column(Integer, CheckConstraint('rating >= 1 AND rating <= 5'))
     desc = Column(String)
     time_cook = Column(Integer)
+    
+    saved_by_user = relationship("SavedRecipe", back_populates="recipe", cascade="all, delete")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    recipe_id = Column(Integer, ForeignKey("recipe.id"))
+
+    user = relationship("User", back_populates="comments")
+    recipe = relationship("Recipe", back_populates="comments")
+
+class SavedRecipe(Base):
+    __tablename__ = "saved_recipe"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    recipe_id = Column(Integer, ForeignKey("recipe.id"))
+
+    user = relationship("User", back_populates="comments")
+    recipe = relationship("Recipe", back_populates="comments")
+
+    
+
+    
